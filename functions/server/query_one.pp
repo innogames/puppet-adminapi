ac841f790d28341965f5d7a208624d74dbfaa712
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-09-25 17:35:27 +0200

    Allow hash on the query functions

--- functions/server/query_one.pp
+++ functions/server/query_one.pp
@@ -4,13 +4,18 @@
 # Copyright (c) 2017, InnoGames GmbH
 #
 function ig::server::query_one (
-    String[1] $query,
-    String[1] $attribute_id = 'fqdn',
+    Variant[String[1], Ig::Igserver_query] $query,
+    Ig::Attribute_id                       $attribute_id = 'hostname',
 ) >> String[1] {
     igserver(ig::format::cli_arguments({
         'one'   => undef,
         'attr'  => $attribute_id,
         'order' => $attribute_id,
-        undef   => $query,
+        undef   => $query ? {
+            String  => $query,
+            default => $query.map |$key, $value| {
+                "${key}=${value}"
+            },
+        },
     }))[0]
 }
---
+++ types/igserver_query.pp
@@ -0,0 +1,6 @@
+#
+# Query for igserver
+#
+# Copyright (c) 2017, InnoGames GmbH
+#
+type Ig::Igserver_query = Hash[Ig::Attribute_id, Variant[Boolan, Numeric, String[1]]]
