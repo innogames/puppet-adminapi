db2b46a12314397e03f84ad42dfa34fe5bcd5c86
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-10-04 14:40:14 +0200

    Show errors on query()

--- lib/puppet/parser/functions/query.rb
+++ lib/puppet/parser/functions/query.rb
@@ -10,12 +10,12 @@ module Puppet::Parser::Functions
   newfunction(:query, :type => :rvalue) do |args|
     raise(Puppet::ParseError, "query(): Wrong number of arguments") if args.size != 2
 
-    stdout = %x[adminapi_query -a #{args[1]} -o #{args[1]} '#{args[0]}'].split(/\n/)
+    output = %x[adminapi_query -a #{args[1]} -o #{args[1]} '#{args[0]}' 2>&1].split(/\n/)
 
     if $? != 0
-        raise(Puppet::Error, "adminapi_query command exit is %s expected 0" % $?)
+        raise(Puppet::Error, "query(): #{output[-1]}")
     end
 
-    return stdout
+    return output
   end
 end
