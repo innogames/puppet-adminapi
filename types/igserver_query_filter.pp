c98c3bc4737e23c333848db613b92f88784da525
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-11-27 12:18:57 +0100

    Accept Regexp(String) filter

--- types/igserver_query_filter.pp
+++ types/igserver_query_filter.pp
@@ -8,7 +8,7 @@
 type Ig::Igserver_query_filter = Variant[
     # The list is ordered relatively from simple to complicated.
     Hash[Enum['Empty'], Undef, 1, 1],
-    Hash[Enum['Regexp'], Regexp, 1, 1],
+    Hash[Enum['Regexp'], String[1], 1, 1],
     Hash[Enum['Any', 'All'], Array[Variant[Numeric, Pattern[/\A[^\(\)\'"]+\Z/], Ig::Igserver_query_filter]], 1, 1],
     Hash[Enum['Not'], Variant[Numeric, Pattern[/\A[^\(\)\'"]+\Z/], Ig::Igserver_query_filter], 1, 1],
 ]
