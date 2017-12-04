cac4f3a5158a1855077147138a84275dfc421ace
Emre Hasegeli <emre.hasegeli@innogames.com>
2017-12-04 11:32:48 +0100

    Add new query filters

--- types/server/query_filter.pp
+++ types/server/query_filter.pp
@@ -11,7 +11,9 @@ type Ig::Server::Query_filter = Variant[
     Numeric,
     Pattern[/\A[^\(\)\'"]+\Z/],
     Hash[Enum['Empty'], Undef, 1, 1],
+    Hash[Enum['GreaterThan', 'GreaterThanOrEquals', 'LessThan', 'LessThanOrEquals'], Variant[Numeric, Pattern[/\A[^\(\)\'"]+\Z/]], 1, 1],
     Hash[Enum['Regexp'], String[1], 1, 1],
+    Hash[Enum['Overlaps', 'ContainedBy', 'ContainedOnlyBy', 'Contains', 'StartsWith'], Pattern[/\A[^\(\)\'"]+\Z/], 1, 1],
     Hash[Enum['Any', 'All'], Array[Ig::Server::Query_filter], 1, 1],
     Hash[Enum['Not'], Ig::Server::Query_filter, 1, 1],
 ]
