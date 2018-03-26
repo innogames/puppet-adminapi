23dbcc23322aaba31fc7c2b1ad6c4fb5e0e5fc4b
Serenus Hagenberg <serenus.hagenberg@innogames.com>
2018-03-26 18:46:12 +0200

    Add monitoring_check helper for non nrpe services

--- functions/server/multiadd.pp
+++ functions/server/multiadd.pp
@@ -6,9 +6,17 @@
 function ig::server::multiadd (
     Ig::Server::Attribute_id                     $attribute_id,
     Variant[Numeric, Pattern[/\A[^\(\)\'"]+\Z/]] $value,
+    Boolean                                      $monitoring_check_call = false,
 ) >> Undef {
     if ! defined(Class['ig::server']) {
         fail('This function can only be used with ig::server.')
+    }
+
+    if $attribute_id == 'monitoring_checks' and ! $monitoring_check_call {
+        notify { "multiadd_${attribute_id}_${value}_command":
+            message  => '"multiadd" is deprecated for attribute monitoring_checks. Use "ig::helper::monitoring_check" instead.',
+            loglevel => 'warning',
+        }
     }
 
     $old_values = getvar("ig::server::${attribute_id}")
