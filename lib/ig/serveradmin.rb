cc50eb26343f0f0a4c1cbd019143562d62df17fe
Emre Hasegeli <emre.hasegeli@innogames.com>
2018-06-06 15:07:42 +0200

    Implement multi attribute maintenance in Ruby

--- lib/ig/serveradmin.rb
+++ lib/ig/serveradmin.rb
@@ -13,47 +13,49 @@ require 'openssl'
 require 'puppet/util/errors'
 
 module Ig::Serveradmin
-    def self.query(filters, restrict, order_by)
+    def self.request(endpoint, payload)
         application_id = Digest::SHA1.hexdigest(ENV['SERVERADMIN_TOKEN'])
         timestamp = Time.now.getutc.to_i.to_s
-        
-        query_json = JSON.generate({
-            'filters' => filters,
-            'restrict' => restrict,
-            'order_by' => order_by,
-        })
+        payload_json = JSON.generate(payload)
+        uri = URI(ENV['SERVERADMIN_BASE_URL'] + endpoint)
 
-        security_token = OpenSSL::HMAC.hexdigest(
-            OpenSSL::Digest.new('sha1'),
-            ENV['SERVERADMIN_TOKEN'],
-            timestamp + ':' + query_json,
-        )
-
-        uri = URI(ENV['SERVERADMIN_BASE_URL'] + '/dataset/query')
         req = Net::HTTP::Get.new(uri.request_uri)
-
         req['Content-Encoding'] = 'application/x-json'
         req['X-Timestamp'] = timestamp
         req['X-Application'] = application_id
-        req['X-SecurityToken'] = security_token
-        req.body = query_json
+        req['X-SecurityToken'] = OpenSSL::HMAC.hexdigest(
+            OpenSSL::Digest.new('sha1'),
+            ENV['SERVERADMIN_TOKEN'],
+            timestamp + ':' + payload_json,
+        )
+        req.body = payload_json
 
-        res = Net::HTTP.start(
-            uri.host,
-            uri.port,
-            :use_ssl => uri.scheme == 'https'
-        ) do |http|
+        res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
             http.request(req)
         end
 
         if ['2', '3'].include?(res.code[0])
             res_json = JSON.parse(res.body)
 
-            if res_json['status'] && res_json['status'] == 'success' && res_json['result']
+            if res_json['status'] && res_json['status'] == 'success'
                 return res_json['result']
             end
         end
+    end
 
-        fail('Serveradmin error "' + res.body + '" with HTTP code ' + res.code + ' for ' + payload_json)
+    def self.query(filters, restrict, order_by)
+        request('/dataset/query', {
+            'filters'  => filters,
+            'restrict' => restrict,
+            'order_by' => order_by,
+        })
+    end
+
+    def self.commit(created, changed, deleted)
+        request('/dataset/commit', {
+            'created' => created,
+            'changed' => changed,
+            'deleted' => deleted,
+        })
     end
 end
---
+++ lib/puppet/functions/ig/server/change_multi_attribute.rb
@@ -0,0 +1,33 @@
+#
+# Serveradmin multiadd function
+#
+# Copyright (c) 2018 InnoGames GmbH
+#
+
+require 'puppet/util/errors'
+
+require_relative '../../../../ig/serveradmin'
+
+Puppet::Functions.create_function(:'ig::server::change_multi_attribute') do
+
+    dispatch :execute do
+        param 'Integer', :object_id
+        param 'Ig::Server::Attribute_id', :attribute
+        param 'Array[Ig::Server::Attribute_value]', :additions
+        param 'Array[Ig::Server::Attribute_value]', :removals
+        return_type 'Undef'
+    end
+
+    def execute(object_id, attribute, additions, removals)
+        Ig::Serveradmin.commit([], [{
+            'object_id' => object_id,
+            attribute   => {
+                'action' => 'multi',
+                'add'    => additions,
+                'remove' => removals,
+            },
+        }], [])
+        
+        nil
+    end
+end
