96731dcd61377a02e02df65126159f1a8688ce5d
Emre Hasegeli <emre.hasegeli@innogames.com>
2018-06-07 12:45:01 +0200

    Provide ig::server arguments via hiera interface

---
+++ lib/puppet/functions/ig/server/hiera_backend.rb
@@ -0,0 +1,23 @@
+#
+# Serveradmin hiera backend
+#
+# Copyright (c) 2018 InnoGames GmbH
+#
+
+require_relative '../../../../ig/serveradmin'
+
+Puppet::Functions.create_function(:'ig::server::hiera_backend') do
+
+    dispatch :execute do
+        param 'Hash', :options
+        param 'Puppet::LookupContext', :context
+    end
+
+    def execute(options, context)
+        results = {}
+        Ig::Serveradmin.query({'hostname' => closure_scope['fqdn']}, options['restrict'], [])[0].each { |key, value|
+            results['ig::server::' + key] = value
+        }
+        results
+    end
+end
