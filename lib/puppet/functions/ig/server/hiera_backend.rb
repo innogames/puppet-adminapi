3a1ef5659dfc7664a881c0ef075f884d08ddc5ec
Patrick Meyer <patrick.meyer@innogames.com>
2019-08-06 12:56:30 +0200

    hiera: Make serveradmin hiera target class configurable

--- lib/puppet/functions/ig/server/hiera_backend.rb
+++ lib/puppet/functions/ig/server/hiera_backend.rb
@@ -7,8 +7,12 @@
 require_relative '../../../../ig/serveradmin'
 
 Puppet::Functions.create_function(:'ig::server::hiera_backend') do
-
     dispatch :execute do
+        param 'Struct[{puppet_class=>String[1], restrict=>Array}]', :options
+        param 'Puppet::LookupContext', :context
+    end
+
+    argument_mismatch :missing_options do
         param 'Hash', :options
         param 'Puppet::LookupContext', :context
     end
@@ -16,8 +20,17 @@ Puppet::Functions.create_function(:'ig::server::hiera_backend') do
     def execute(options, context)
         results = {}
         Ig::Serveradmin.query({'hostname' => closure_scope['trusted']['certname']}, options['restrict'], [])[0].each { |key, value|
-            results['ig::server::' + key] = value
+            results[options['puppet_class'] + '::' + key] = value
         }
         results
     end
+
+  def missing_options(options, context)
+    if !(options.key? 'puppet_class' and options['puppet_class'].is_a? String)
+        err_msg = "'puppet_class' must be declared as a String in hiera.yaml when using this data_hash function"
+    else
+        err_msg = "'restrict' must be declared as an Array in hiera.yaml when using this data_hash function"
+    end
+    err_msg
+  end
 end
